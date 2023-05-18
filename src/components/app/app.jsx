7e2e4442e0c499f2a4e06e9052afc5398d522c74
lucy2n <lyusianaumenko@yandex.by>
2023-05-18 13:19:38 +0300

    feat: добавлен component appHeader

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,15 +1,11 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
+import AppHeader from '../appHeader/AppHeader';
 
 function App() {
   return (
     <div className={styles.app}>
-      <pre style={{
-      	margin: "auto",
-      	fontSize: "1.5rem"
-      }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
-      </pre>
+      <AppHeader />
     </div>
   );
 }
