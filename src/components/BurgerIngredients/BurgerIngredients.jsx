44fbcdfb1fbcdc058dfe79551b0de1e37f4e840a
lucy2n <lyusianaumenko@yandex.by>
2023-06-24 22:55:23 +0300

    feat: добавлено переключение табов по мере скрола

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -8,14 +8,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { getIngredients } from "../../services/actions/ingredients";
 import { ADD_INGREDIENT } from '../../services/actions/constructor';
 import { INGREDIENT_MODAL, OPEN_MODAL } from '../../services/actions/modal';
+import { useInView, InView } from 'react-intersection-observer';
 
 function BurgerIngredients() {
     const { ingredients, ingredientsRequest, ingredientsFailed }  = useSelector(state => state.ingredients)
     const { modalType, modalProps } = useSelector(state => state.modal)
 
+    const [ scrollPosition, setScrollPosition ] = React.useState(0);
+
     const dispatch = useDispatch();
-  
-    const [current, setCurrent] = React.useState('Булки');
 
     React.useEffect(() => {
         dispatch(getIngredients());
@@ -33,36 +34,71 @@ function BurgerIngredients() {
         })
     }
 
+    const bunTab = "buns";
+    const mainTab = "main";
+    const sauceTab = "sauce";
+
+    const [activeTab, setActiveTab] = React.useState(bunTab);
+
+    React.useEffect(() => {
+        const scrollWrapper = document.querySelector(".custom-scroll");
+        scrollWrapper.addEventListener("scroll", handleScroll, { passive: true });
+    
+        return () => {
+            scrollWrapper.removeEventListener("scroll", handleScroll);
+        };
+    }, [scrollPosition]);
+    
+    const handleScroll = () => {
+        // Создание объекта типа "вкладка": "расстояние до верха"
+        const tabsDistance = {
+            [bunTab]: getDistance(`.${bunTab}`),
+            [sauceTab]: getDistance(`.${sauceTab}`),
+            [mainTab]: getDistance(`.${mainTab}`),
+        }
+
+        // Сортировка
+        const sortedTabs = Object.keys(tabsDistance).sort((a, b) => {
+            return tabsDistance[a] - tabsDistance[b]
+        }) 
+        setActiveTab(sortedTabs[0])
+    };
+
+    const getDistance = (className) => {
+        return Math.abs(document.querySelector(className).getBoundingClientRect().top - 281);
+    }
+
     return(
-        <div>
+        <>
             <div className={`mb-10 ${ingredientsStyles.tabs}`}>
-                <Tab value="one" active={current === 'Булки'} onClick={setCurrent}>
+                <Tab value="one" active={activeTab === bunTab} onClick={setActiveTab}>
                     Булки
                 </Tab>
-                <Tab value="two" active={current === 'Соусы'} onClick={setCurrent}>
+                <Tab value="two" active={activeTab === sauceTab} onClick={setActiveTab}>
                     Соусы
                 </Tab>
-                <Tab value="three" active={current === 'Начинки'} onClick={setCurrent}>
+                <Tab value="three" active={activeTab === mainTab} onClick={setActiveTab}>
                     Начинки
                 </Tab>
             </div>
             <div className={`custom-scroll ${ingredientsStyles.wrapper}`}>
-                <div className='mb-10'>
+
+                <div className={`mb-10 ${bunTab}`}>
                     <p className="text text_type_main-medium mb-6">Булки</p>
                     <ul className={ingredientsStyles.ul}>
                     {
-                         ingredients.filter(ingredient => ingredient.type === "bun").map((ingredient) => (
+                        ingredients.filter(ingredient => ingredient.type === "bun").map((ingredient) => (
                             <li className={ingredientsStyles.li} key={ingredient._id}>
                                 <Ingredient
                                 ingredient={ingredient}
                                 openIngredientDetails={openIngredientDetails}
                                 />
                             </li>
-                         ))
+                        ))
                     }
                     </ul>
                 </div>
-                <div className='mb-10'>
+                <div className={`mb-10 ${sauceTab}`}>
                     <p className="text text_type_main-medium mb-6">Соусы</p>
                     <ul className={ingredientsStyles.ul}>
                     {
@@ -77,7 +113,7 @@ function BurgerIngredients() {
                     }
                     </ul>
                 </div>
-                <div className='mb-10'>
+                <div className={`mb-10 ${mainTab}`}>
                     <p className="text text_type_main-medium mb-6">Начинки</p>
                     <ul className={ingredientsStyles.ul}>
                     {
@@ -98,7 +134,7 @@ function BurgerIngredients() {
                     <IngredientDetails />
                 </Modal> 
             }
-        </div>
+        </>
     )
 }
 
