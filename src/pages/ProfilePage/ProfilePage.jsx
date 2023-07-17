a6517a8c56e01fe03b2b8211d0c56a65f62256cb
lucy2n <lyusianaumenko@yandex.by>
2023-07-17 18:31:48 +0300

    fix: рефактор кода

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -17,7 +17,7 @@ export const ProfilePage = () => {
                     className={`text text_type_main-medium pt-4 pr-10 pb-4 pl-10 ${location.pathname === RoutePathname.profilePage ? styles.tab_active : styles.tab}`}>Профиль</Link>
                     <Link 
                     to="/profile/orders" 
-                    className={`text text_type_main-medium pt-4 pr-10 pb-4 pl-10 ${location.pathname === RoutePathname.ordersPage ? styles.tab_active : styles.tab}`}> История заказов</Link>
+                    className={`text text_type_main-medium pt-4 pr-10 pb-4 pl-10 ${location.pathname === '/profile/orders' ? styles.tab_active : styles.tab}`}> История заказов</Link>
                     <a onClick={() => dispatch(logout())} className={`text text_type_main-medium pt-4 pr-10 pb-4 pl-10 ${ styles.tab }`}>Выход</a>
                 </nav>
                 <p className={`text text_type_main-small text_color_inactive pt-4 pb-4 pl-10 ${styles.text_color_dark}`}> В этом разделе вы можете изменить свои персональные данные</p>
